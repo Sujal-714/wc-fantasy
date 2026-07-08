f835d87470846624f9c6280ad3d5d97b455663b2
Sujal Poudel <sujalpoudel714@gmail.com>
2026-07-08 12:17:29 +0545

    Fix: Match Points Fronetend

--- server/src/controllers/matches.ts
+++ server/src/controllers/matches.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import pool from '../db'
+import { AuthRequest } from '../middleware/auth'
 
 export const matches = async (req: Request, res: Response) => {
   try {
@@ -16,3 +17,32 @@ export const matches = async (req: Request, res: Response) => {
   }
 }
 
+export const getMyPoints = async (req: AuthRequest, res: Response) => {
+  const { matchId } = req.params
+  const userId = req.userId
+
+  try {
+    const result = await pool.query(
+      `SELECT 
+         p.name, p.position, p.country,
+         pms.goals, pms.assists, pms.minutes_played,
+         pms.clean_sheet, pms.yellow_cards, pms.red_cards,
+         pms.saves, pms.penalties_missed, pms.fantasy_points,
+         tp.is_captain, tp.player_id
+       FROM player_match_stats pms
+       JOIN players p ON pms.player_id = p.id
+       JOIN team_players tp ON tp.player_id = p.id
+       JOIN teams t ON tp.team_id = t.id
+       WHERE pms.match_id = $1
+       AND t.user_id = $2
+       ORDER BY pms.fantasy_points DESC`,
+      [matchId, userId]
+    )
+
+    const totalPoints = result.rows.reduce((sum: number, r: any) => sum + r.fantasy_points, 0)
+    res.json({ players: result.rows, totalPoints })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Server error' })
+  }
+}
--- server/src/routes/matches.ts
+++ server/src/routes/matches.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { matches } from '../controllers/matches'
+import { matches , getMyPoints} from '../controllers/matches'
+import { authenticate } from '../middleware/auth'
 const router = Router()
 
 
 router.get('/', matches);
+router.get('/:matchId/my-points', authenticate, getMyPoints)  
 
 export default router
